d37d0ee34f1093988968faf87f677e16edf729be
Maxim Belitskiy <48838052+SansenSekai@users.noreply.github.com>
2023-07-03 00:37:56 +0300

    Initial commit

---
+++ src/main/java/com/inovusbot/mytestbot/MyTestBotApplication.java
@@ -0,0 +1,13 @@
+package com.inovusbot.mytestbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTestBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTestBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inovusbot/mytestbot/MyTestBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inovusbot.mytestbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTestBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
