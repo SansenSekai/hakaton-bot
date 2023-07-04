b3f7e26ee4a1d3e4a527ec2c1e5018844a0448cb
Maxim Belitskiy <48838052+SansenSekai@users.noreply.github.com>
2023-07-04 05:54:40 +0300

    change port

--- src/main/java/com/inovusbot/mytestbot/module/auth/service/AuthService.java
+++ src/main/java/com/inovusbot/mytestbot/module/auth/service/AuthService.java
@@ -4,7 +4,6 @@ import com.inovusbot.mytestbot.service.MessageSenderService;
 import com.inovusbot.mytestbot.service.UserService;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
