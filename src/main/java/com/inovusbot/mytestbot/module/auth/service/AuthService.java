5e1fbf9cccb41c8a7046255a05e072c397b545de
Maxim Belitskiy <48838052+SansenSekai@users.noreply.github.com>
2023-07-04 06:03:11 +0300

    change port

--- src/main/java/com/inovusbot/mytestbot/module/auth/service/AuthService.java
+++ src/main/java/com/inovusbot/mytestbot/module/auth/service/AuthService.java
@@ -16,7 +16,7 @@ import static com.inovusbot.mytestbot.config.Commands.ABOUT;
 @Service
 public class AuthService {
     // @Value("${oauth_redirect_url}")
-    private String redirectUrl = "https://i-knowus-santoryu1001.amvera.io:80/login";
+    private String redirectUrl = "https://i-knowus-santoryu1001.amvera.io:8080/login";
     private final MessageSenderService telegramBot;
     private final UserService userService;
 
